d75c1ba300bc5fa0f947a71a7ef253bab7781b36
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2023-04-11 18:41:00 -0500

    Make the menu

--- LinkedLists.UI/Program.cs
+++ LinkedLists.UI/Program.cs
@@ -1,18 +1,44 @@
 ﻿using LinkedLists.Logic;
+using System.ComponentModel.Design;
 
 Console.WriteLine("¨*** Linked Lists ***");
 var singleList = new SingleList<string>();
-singleList.Add("Juan");
-singleList.Add("Ledys");
-singleList.Add("Geralin");
-singleList.Add("Ronal");
-singleList.Add("Valery");
-singleList.Add("Benedict");
-singleList.Add("Isis");
-singleList.Add("Gaia");
-singleList.Add("Toño");
-singleList.Add("Otto");
-singleList.Add("Freya");
-singleList.Add("Alma");
-singleList.Add("Johany");
-Console.WriteLine(singleList);
+
+int option;
+do
+{
+    option = Menu();
+    switch (option)
+    {
+        case 1: AddItem(); break;
+        case 2: ShowList(); break;
+    }
+} while (option != 0);
+
+void ShowList()
+{
+    if (singleList.IsEmpty)
+    {
+        Console.WriteLine("The list is empty.");
+        return;
+    }
+    Console.WriteLine("The elements are: ");
+    Console.WriteLine(singleList);
+}
+
+void AddItem()
+{
+    Console.Write("Enter the item: ");
+    var item = Console.ReadLine();
+    singleList.Add(item!);
+}
+
+int Menu()
+{
+    Console.WriteLine("1. Add item.");
+    Console.WriteLine("2. Show List.");
+    Console.WriteLine("0. Exit.");
+    Console.Write("Enter your choice: ");
+    var option = Console.ReadLine();
+    return Convert.ToInt32(option); 
+}
